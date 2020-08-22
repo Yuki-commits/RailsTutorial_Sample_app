58e84ab75e4423b6c6cdb269476a3f757c007865
Yuuki <basket_ball.23.y_k@icloud.com>
2020-08-22 23:31:27 +0900

    Finish user sighup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,4 +19,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", "The form contains 4 errors."
     assert_select "div#error_explanation li", 4
   end
+
+  test "valid signup infomation" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post signup_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash[:success].empty?
+  end
 end
